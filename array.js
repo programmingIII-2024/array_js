749bfb38c9b759717c2e05bb7175d8e7369817ca
ohba <ohba@sendai-nct.ac.jp>
2025-01-07 17:21:32 +0900

    First commit

---
+++ array.js
@@ -0,0 +1,25 @@
+function array_print()
+{
+
+	let str_array = ["One", "Two", "Three"];
+	let num_array = [ 1, 2, 3];
+
+	let out_array=str_array;	// 配列の代入は1行で全要素が入る
+//	let out_array=num_array;
+
+	alert(out_array);			// 配列名を指定すると全要素
+	alert(out_array[0]);		// 配列の参照番号は0から
+	alert(out_array[1]);
+	alert(out_array[2]);
+
+	// 配列名.lengthで配列数の取り出し
+	alert("配列の要素数は"+out_array.length +"です");
+
+	// 配列の追加は追加要素にそのまま代入
+	/*	
+	out_array[3]="Four";
+	alert(out_array.length +"個目の配列要素は" +out_array[3]);
+	*/
+
+
+}
